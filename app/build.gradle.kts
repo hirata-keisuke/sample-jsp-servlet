c680cb603db0538eda467db13690eea1e4047c98
hirata-keisuke <plainpeace39th@gmail.com>
2024-05-06 01:39:25 +0900

    libs.guavaを除去

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,6 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.guava)
-    
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
     compileOnly("jakarta.servlet.jsp:jakarta.servlet.jsp-api:3.1.1")
 }
